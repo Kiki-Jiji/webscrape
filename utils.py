a4dc59c960d1a75c25f951f4e7513ac2774af217
Joshua <joshuah@gmail.com>
2022-04-19 23:45:02 +0100

    v1 finished

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 from ipaddress import ip_address
 from itertools import count
 import requests
+from requests.exceptions import ProxyError
 import random
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import pandas as pd
+from datetime import date
+import gspread
+import df2gspread as d2g
+import logging
 
 def get_child(html, pos):
     return [i for i in html[pos].children][0]
@@ -12,6 +17,7 @@ def extract_books(page):
 
     book_list = page.find_all('span', {'class' : 'zg-bdg-text'})
 
+    assert len(book_list) > 0, 'no books found'
     books = {}
 
     for book_ranking in book_list:
@@ -27,25 +33,49 @@ def extract_books(page):
 
         book_info = [i for i in [i for i in [i for i in book_box.children][1]][0]]
 
-        assert len(book_info) == 6
+        if len(book_info) != 6:
+            if len(book_info) == 5:
+                # gonna guess that 5 means no star rating TODO improve later
 
-        img = book_info[0]
-        book_title = book_info[1]
-        author = book_info[2]
-        star_rating = book_info[3]
-        book_type = book_info[4]
-        price = book_info[5]
+                img = book_info[0]
+                book_title = book_info[1]
+                author = book_info[2]
+                book_type = book_info[3]
+                price = book_info[4]
 
-        # book title
-        book['book_title_txt'] = [i for i in [i for i in [i for i in book_title.children][0].children][0].children][0]
-        book['author_txt'] = [i for i in [i for i in [i for i in author][0]][0]][0]
-        book['star_rating_txt' ]= [i for i in [i for i in [i for i in [i for i in [i for i in star_rating][0]][0]][0]][0]][0]
-        book['book_type_txt'] = [i for i in [i for i in book_type][0]][0]
-        book['price_txt'] = [i for i in [i for i in [i for i in [i for i in price][0]][0]][0]][0]
+                # book title
+                book['book_title_txt'] = [i for i in [i for i in [i for i in book_title.children][0].children][0].children][0]
+                book['author_txt'] = [i for i in [i for i in [i for i in author][0]][0]][0]
+                book['star_rating_txt' ] = 'unknown'
+                book['book_type_txt'] = [i for i in [i for i in book_type][0]][0]
+                book['price_txt'] = [i for i in [i for i in [i for i in [i for i in price][0]][0]][0]][0]
+
+            else:
+                # no idea jk :P but dict always needs 5 keys
+                book['book_title_txt'] = 'unknown'
+                book['author_txt'] = 'unknown'
+                book['star_rating_txt' ] = 'unknown'
+                book['book_type_txt'] = 'unknown'
+                book['price_txt'] = 'unknown'
+        else:
+            # expected 6 catorgories
+            img = book_info[0]
+            book_title = book_info[1]
+            author = book_info[2]
+            star_rating = book_info[3]
+            book_type = book_info[4]
+            price = book_info[5]
+
+            # book title
+            book['book_title_txt'] = [i for i in [i for i in [i for i in book_title.children][0].children][0].children][0]
+            book['author_txt'] = [i for i in [i for i in [i for i in author][0]][0]][0]
+            book['star_rating_txt' ]= [i for i in [i for i in [i for i in [i for i in [i for i in star_rating][0]][0]][0]][0]][0]
+            book['book_type_txt'] = [i for i in [i for i in book_type][0]][0]
+            book['price_txt'] = [i for i in [i for i in [i for i in [i for i in price][0]][0]][0]][0]
 
         books[book_rank_scrape] = book
 
-        return books
+    return books
 
 
 
@@ -85,3 +115,85 @@ def get_free_proxies():
         count += 1
 
     return pd.DataFrame.from_dict(rows, orient='index')
+
+
+
+class Proxy:
+    def __init__(self, HEADERS) -> None:
+        self.headers = HEADERS
+        self.proxies = get_free_proxies()
+        self.proxy_num = 0
+        self.current_proxy = self.proxies.ip.array[self.proxy_num]
+
+    def get_webpage(self, url):
+        success = False
+        while success is False:
+            print(self.current_proxy)
+            try:
+                webpage = self.try_proxy(url)
+                logging.debug(f'Success with {self.current_proxy}, proxy number {self.proxy_num}')
+                success = True
+            except:
+                self.set_current_proxy()
+        return webpage
+
+    def set_current_proxy(self):
+        self.proxy_num += 1
+        self.current_proxy = self.proxies.ip.array[self.proxy_num]
+
+    def try_proxy(self, url):
+
+        i = 1
+        n = 5
+        success_status = False
+        while success_status is False:
+            print(success_status)
+            try:
+                webpage = requests.get(url, headers=self.headers , proxies={"http": self.current_proxy, "https": self.current_proxy})
+                page = BeautifulSoup(webpage.content, features= "html.parser")
+                
+                if amazon_blocked(page):
+                    if i < n:
+                        i += 1
+                    else:
+                        success_status = True
+                success_status = True
+            except ProxyError:
+                if i < n:
+                    i += 1
+                else:
+                    success_status = True
+        
+        assert i < n, "Proxy failed"
+
+        return webpage
+
+
+def write_gs(df):
+
+    gc = gspread.service_account("webscrape-346716-e7082b6f73c5.json")
+
+    sh = gc.open("Amazon Data")
+
+    sheetName = date.today().strftime("%d_%m_%Y") 
+
+    try:
+        sh.add_worksheet(sheetName, rows = 20, cols = 10)
+    except:
+        sheetName = sheetName + '_1'
+        sh.add_worksheet(sheetName, rows = 20, cols = 10)
+
+    logging.debug(f'Created excel sheetnamer was: {sheetName}')
+
+    worksheet = sh.worksheet(sheetName)
+
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+
+
+
+
+def amazon_blocked(page):
+
+    comments = page.find_all(string=lambda text: isinstance(text, Comment))
+
+    return True in ['automated' in i for i in comments]
--- webscape.py
+++ webscape.py
@@ -1,18 +1,22 @@
-from ast import Try
-import imp
-from bs4 import BeautifulSoup
-import requests
 import pandas as pd
-import gspread
-import df2gspread as d2g
-from datetime import date
+from datetime import date, datetime
 import time
 from random import random
+import logging
 
-from utils import extract_books, get_free_proxies
+from utils import extract_books, get_free_proxies, Proxy, write_gs
+
+today = date.today().strftime("%d_%m_%Y")
+
+logging.basicConfig(
+    level=logging.DEBUG, 
+    filename = f'logs/app_{today}.log', 
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s')
 
 
-
+time_start = datetime.now()
+logging.debug(f'Time started {time_start}')
 
 HEADERS = { 
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36', 
@@ -22,13 +26,6 @@ HEADERS = {
 'DNT' : '1', # Do Not Track Request Header 
 'Connection' : 'close'
 }
-
-today = date.today().strftime("%d_%m_%Y")
-
-gc = gspread.service_account("webscrape-346716-e7082b6f73c5.json")
-
-sh = gc.open("Amazon Data")
-
 
 urls = {
     'UK_Hist_Romance': 'https://www.amazon.co.uk/Best-Sellers-Kindle-Store-Historical-Romance/zgbs/digital-text/362727031/ref=zg_bs_unv_digital-text_4_3507148031_2',
@@ -42,31 +39,38 @@ urls = {
 }
 
 
-proxies = get_free_proxies()
+# urls = {
+#     'UK_Hist_Romance': 'https://www.amazon.co.uk/Best-Sellers-Kindle-Store-Historical-Romance/zgbs/digital-text/362727031/ref=zg_bs_unv_digital-text_4_3507148031_2'
+# }
 
-ips = proxies.ip.array
+Prox = Proxy(HEADERS)
 
-proxy = ips[0]
+book_pages = {}
 
 for url in urls:
-    
     time.sleep(random() / random()) 
 
-    webpage = requests.get(urls[url], headers=HEADERS, proxies={"http": proxy, "https": proxy})
-    page = BeautifulSoup(webpage.content, features= "html.parser")
+    page = Prox.get_webpage(
+        url= urls[url],
+    )
+
+    logging.debug(f'Scraped {url}')
 
     books = extract_books(page)
 
-    books_df = pd.DataFrame.from_dict(books, orient='index')
+    df =  pd.DataFrame.from_dict(books, orient='index')
+    df['category'] = url
 
-    sheetName = today + '_' + url
+    book_pages[url] = df
 
-    try:
-        sh.add_worksheet(sheetName, rows = 20, cols = 10)
-    except:
-        sheetName = sheetName + '1'
-        sh.add_worksheet(sheetName, rows = 20, cols = 10)
 
-    worksheet = sh.worksheet(sheetName)
+all_webpages_df =  pd.concat(book_pages.values(), ignore_index=True)
 
-    worksheet.update([books_df.columns.values.tolist()] + books_df.values.tolist())
+try:
+    write_gs(all_webpages_df)
+except:
+    all_webpages_df.to_csv(f'backup_data_{today}.csv', index=False)
+
+time_end = datetime.now()
+logging.debug(f'Time end {time_end}')
+logging.debug(f'Time taken {time_end - time_start}')
